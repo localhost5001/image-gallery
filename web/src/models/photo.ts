5373cdb45257fe5aeb59236a861e2b0921a5eb5e
Leo Kim <e_leokim@yahoo.com>
2022-06-18 18:00:20 -0700

    web: add models

---
+++ web/src/models/photo.ts
@@ -0,0 +1,9 @@
+export interface Photo {
+    id: number
+    photographer_id: number
+    collection_id: number
+    string: number
+    description: string
+    height: number
+    width: number
+}
