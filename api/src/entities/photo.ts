5cdc45c835b832f058f6e25ca535c7128caa1f3c
Leo Kim <e_leokim@yahoo.com>
2022-06-20 18:39:23 -0700

    api: define base entities

---
+++ api/src/entities/photo.ts
@@ -0,0 +1,9 @@
+export interface Photo {
+    id: number
+    photographer_id: number
+    collection_id: number
+    url: string
+    description?: string
+    height: number
+    width: number
+}
---
+++ api/src/entities/photographer.ts
@@ -0,0 +1,9 @@
+export interface Photographer {
+    id: number
+    name: string
+    location: string
+    bio: string
+    profile_image_url: string
+    twitter_username: string
+    instagram_username: string
+}
