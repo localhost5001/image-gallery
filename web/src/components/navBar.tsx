162db8ce71562ad0f2fd13579bba38baad839310
Leo Kim <e_leokim@yahoo.com>
2022-06-21 15:28:32 -0700

    web: optimize navbar

--- web/src/components/navBar.tsx
+++ web/src/components/navBar.tsx
@@ -7,12 +7,22 @@ import SearchBtn from 'components/searchBtn'
 export default function NavBar() {
     return (
     <nav className="bg-gray-800 py-6">
-        <div className="max-w-6xl mx-auto h-full flex items-center space-x-10">
-            <div className='mx-auto'>
-                <img src={logo} className="mr-3 h-10 mx-1"></img>
+        <div 
+            className='
+                md:max-w-6xl md:space-x-10 md:mx-auto md:flex
+                block justify-center
+            '
+        >
+            <div className='flex justify-center'>
+                <img src={logo} className="mx-5 h-10"></img>
             </div>
 
-            <div className='hidden md:flex flex-grow space-x-4'>
+            <div 
+                className='
+                    md:flex md:flex-grow md:space-x-4 md:sm:space-y-0 md:mt-0
+                    block mx-2 space-y-5 mt-4 flex-grow-0
+                '
+            >
                 <div className="basis-2/5">
                     <TextBox />
                 </div>
@@ -24,20 +34,6 @@ export default function NavBar() {
                 <div className="basis-1/5">
                     <SearchBtn />
                 </div>
-            </div>
-        </div>
-
-        <div className='md:hidden block mx-2 space-y-5 mt-4'>
-            <div className="">
-                <TextBox />
-            </div>
-
-            <div className="">
-                <SelectBox />
-            </div>
-
-            <div className="">
-               <SearchBtn />
             </div>
         </div>
     </nav>    
