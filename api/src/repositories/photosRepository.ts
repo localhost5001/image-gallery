d85c39e1a2197121019029cfd9bf4b4887d4ab7d
Leo Kim <e_leokim@yahoo.com>
2022-06-26 11:22:33 -0700

    api: photosRepository: getPhotos: allow conditiona parameters

--- api/src/repositories/photosRepository.ts
+++ api/src/repositories/photosRepository.ts
@@ -7,11 +7,13 @@ export interface GetPhotosQuery {
 }
 
 export const getPhotos = async (queryParams: GetPhotosQuery): Promise<Photo[]> => {
+    const keyword = queryParams.keyword ? `%${queryParams.keyword}` : undefined
+
     const dbResult = await query<Photo>(`
         SELECT id, photographer_id, collection_id, url, description, height, width
         FROM photo
-        WHERE collection_id = ${queryParams.collectionId} AND description LIKE '%${queryParams.keyword}%'
-    `)
+        WHERE ($1::int IS NULL OR collection_id = $1) AND ($2::text IS NULL OR description ILIKE $2::text)
+    `, [queryParams.collectionId, keyword])
 
     return dbResult.rows
 }
